463fd61f73516e5435a898f15684d6eb011f59af
Richard Tran <rtran4@mail.valenciacollege.edu>
2016-04-22 00:47:39 -0400

    Kitty Photo

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -7,8 +7,28 @@ LOGO_FILENAME = 'logo.png'
 logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
 
-#TODO: Loop over all files in the working directory
-#TODO: Check if file image needs to be
-#TODO: Calculate the new width and height to resize to.
-#TODO: Add the logo.
-#TODO: Save changes.
+os.makedirs("WithLogo", exist_ok = True)
+
+for filename in os.listdir(os.path.join('.','originals')):
+    if not (filename.endswith ('.png') or filename.endswith ('.jpg')) or filename == LOGO_FILENAME:
+        continue
+    
+    im = Image.open(os.path.join('originals',filename))
+    width, height = im.size
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SQUARE:
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+           width = int((SQUARE_FIT_SIZE / height) * width)
+           height = SQUARE_FIT_SIZE
+
+print('Resizing %s...' % (filename))
+im = im.resize((width, height))
+
+print('Adding logo to %s...' % (filename))
+im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+im.save(os.path.join('withLogo', filename))
+
+    
+
